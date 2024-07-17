c4e7badc0dd82308e14df89cd1d2354c8746c504
weiruyi <1581778251@qq.com>
2024-07-17 12:04:52 +0800

    单元测试

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -5,15 +5,16 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDTO;
 import com.xuecheng.content.model.po.CourseBase;
-import com.xuecheng.content.service.CourseService;
+import com.xuecheng.content.service.CourseBaseService;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Api(tags = "课程管理")
@@ -22,14 +23,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CourseBaseInfoController {
 
-    private final CourseService courseService;
+    private final CourseBaseService courseBaseService;
 
     @ApiOperation("课程查询")
-    @GetMapping("/course/list")
+    @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams,@RequestBody(required = false) QueryCourseParamsDTO queryCourseParamsDTO){
         log.info("course");
-        CourseBase courseBase = courseService.getById(1L);
+        CourseBase courseBase = courseBaseService.getById(1L);
         log.info("courseBase:{}",courseBase);
-        return null;
+
+        List<CourseBase> courseBases = new ArrayList<>();
+        courseBases.add(courseBase);
+        PageResult<CourseBase> pageResult = new PageResult<CourseBase>(courseBases, 10, 1, 10);
+        return pageResult;
     }
 }
