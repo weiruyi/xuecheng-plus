86af0515479ad4e55493acad3c6efdf4222b8ab3
weiruyi <1581778251@qq.com>
2024-07-18 17:29:49 +0800

    课程种类查询mapper-树形表

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -6,20 +6,17 @@ import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDTO;
 import com.xuecheng.content.model.po.CourseBase;
 import com.xuecheng.content.service.CourseBaseInfoService;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Api(tags = "课程管理")
 @RestController
-@RequestMapping("/content")
 @RequiredArgsConstructor
 public class CourseBaseInfoController {
 
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -1,9 +1,22 @@
 package com.xuecheng.content.api;
 
 
+import com.xuecheng.content.model.dto.CourseCategoryTreeDTO;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
+@Api(tags = "课程种类管理")
+@RequiredArgsConstructor
 @RestController
 public class CourseCategoryController {
+
+    @GetMapping("/course-category/tree-nodes")
+    public CourseCategoryTreeDTO queryTreeNodes(){
+
+        return null;
+    }
 }
