4c1b62d7ab9d0b4dd1071dda2289e726428a3fbe
weiruyi <1581778251@qq.com>
2024-07-19 11:25:50 +0800

    课程种类查询接口完成

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseCategoryController.java
@@ -2,11 +2,14 @@ package com.xuecheng.content.api;
 
 
 import com.xuecheng.content.model.dto.CourseCategoryTreeDTO;
+import com.xuecheng.content.service.CourseCategoryService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @Api(tags = "课程种类管理")
@@ -14,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CourseCategoryController {
 
-    @GetMapping("/course-category/tree-nodes")
-    public CourseCategoryTreeDTO queryTreeNodes(){
+    private final CourseCategoryService courseCategoryService;
 
-        return null;
+    @GetMapping("/course-category/tree-nodes")
+    public List<CourseCategoryTreeDTO> queryTreeNodes(){
+
+        List<CourseCategoryTreeDTO> courseCategoryTreeDTOS = courseCategoryService.queryTreeNodes("1");
+        return courseCategoryTreeDTOS;
+
     }
 }
