7d289169875ecbb0363b24a233b77407a9684f2a
weiruyi <1581778251@qq.com>
2024-08-02 15:06:47 +0800

    支付宝接口测试

--- xuecheng-plus-orders/xuecheng-plus-orders-service/src/test/java/com/xuecheng/orders/Test1.java
+++ xuecheng-plus-orders/xuecheng-plus-orders-service/src/test/java/com/xuecheng/orders/Test1.java
@@ -1,8 +1,18 @@
 package com.xuecheng.orders;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradeQueryRequest;
+import com.alipay.api.response.AlipayTradeQueryResponse;
+import com.xuecheng.orders.config.AlipayConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
 
 /**
  * @description TODO
@@ -13,10 +23,38 @@ import org.springframework.boot.test.context.SpringBootTest;
  @SpringBootTest
 public class Test1 {
 
+    @Value("${pay.alipay.APP_ID}")
+    String APP_ID;
+    @Value("${pay.alipay.APP_PRIVATE_KEY}")
+    String APP_PRIVATE_KEY;
 
-  @Test
- public void test(){
+    @Value("${pay.alipay.ALIPAY_PUBLIC_KEY}")
+    String ALIPAY_PUBLIC_KEY;
 
-  }
+
+    @Test
+    public void queryPayResult() throws AlipayApiException {
+        AlipayClient alipayClient = new DefaultAlipayClient(AlipayConfig.URL, APP_ID, APP_PRIVATE_KEY, "json", AlipayConfig.CHARSET, ALIPAY_PUBLIC_KEY, AlipayConfig.SIGNTYPE); //获得初始化的AlipayClient
+        // 构造请求参数以调用接口
+        AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", "20150320010101002");
+        //bizContent.put("trade_no", "2014112611001004680073956707");
+        request.setBizContent(bizContent.toString());
+        AlipayTradeQueryResponse response = alipayClient.execute(request);
+        if (response.isSuccess()) {
+            System.out.println("调用成功");
+            String resultJson = response.getBody();
+            //转map
+            Map resultMap = JSON.parseObject(resultJson, Map.class);
+            Map alipay_trade_query_response = (Map) resultMap.get("alipay_trade_query_response");
+            //支付结果
+            String trade_status = (String) alipay_trade_query_response.get("trade_status");
+            System.out.println(trade_status);
+        } else {
+            System.out.println("调用失败");
+        }
+
+    }
 
 }
